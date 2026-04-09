a68a2e3b45a8318d65d9fcf52f7997a3146973ea
tm0e83 <tm0e83@gmail.com>
2026-04-09 11:15:40 +0200

    animations

--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -1,19 +1,92 @@
 import styles from './StartPage.module.css';
 import profileImage from '@/assets/images/timo-rs.png';
 import ProjectCard from '@/components/ProjectCard';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import SplitType from 'split-type';
+
+gsap.registerPlugin(useGSAP);
 
 export default function StartPage() {
+  const nameTextRef = useRef(null);
+  const roleTextRef = useRef(null);
+  const socialIconsRef = useRef(null);
+  const projectsRef = useRef(null);
+
+  useGSAP(() => {
+    if (nameTextRef.current === null) return;
+
+    const split = new SplitType(nameTextRef.current, { types: 'chars' });
+
+    gsap.from(split.chars, {
+      x: 10,
+      opacity: 0,
+      duration: 0.5,
+      stagger: 0.03,
+      ease: 'ease.out(1.5)',
+    });
+
+    return () => split.revert();
+  }, { scope: nameTextRef });
+
+  useGSAP(() => {
+    if (roleTextRef.current === null) return;
+
+    const split = new SplitType(roleTextRef.current, { types: 'chars' });
+
+    gsap.from(split.chars, {
+      x: 10,
+      opacity: 0,
+      duration: 0.5,
+      delay: 0.4,
+      stagger: 0.03,
+      ease: 'ease.out(1.5)',
+    });
+
+    return () => split.revert();
+  }, { scope: roleTextRef });
+
+  useGSAP(() => {
+    if (socialIconsRef.current === null) return;
+
+    gsap.from('a', {
+      opacity: 0,
+      scale: 0,
+      y: 25,
+      delay: 0.5,
+      rotationX: -360,
+      duration: 0.75,
+      stagger: 0.25,
+      ease: 'ease.out(3.5)',
+    });
+  }, { scope: socialIconsRef });
+
+  useGSAP(() => {
+    if (projectsRef.current === null) return;
+
+    gsap.from('a', {
+      y: 75,
+      opacity: 0,
+      // scale: 0,
+      delay: 0.25,
+      duration: 0.65,
+      stagger: 0.30,
+      ease: 'power4.out',
+    });
+  }, { scope: projectsRef });
+
   return (
     <div className={styles.startPage}>
       <div className={styles.content}>
         <div className={styles.profile}>
           <div className={styles.profileImage}>
-            <img src={profileImage} className={styles.profileImage} alt="Timo Ribeiro Szyszka" />
+            <img src={profileImage} className={styles.profileImage} alt="Timo Ribeiro Szyszka" />          
           </div>  
           <div className={styles.description}>
-            <div className={styles.name}>Timo Ribeiro Szyszka</div>
-            <div className={styles.role}>Frontend-Developer</div>
-            <div className={styles.social}>
+            <div className={styles.name} ref={nameTextRef}>Timo Ribeiro Szyszka</div>
+            <div className={styles.role} ref={roleTextRef}>Frontend-Developer</div>
+            <div className={styles.social} ref={socialIconsRef}>
               <a href="https://github.com/tm0e83" title="View on Github" target="_blank">
                 <img 
                   src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20width='24'%20height='24'%20viewBox='0%200%2024%2024'%3E%3Cpath%20fill='%23000000'%20d='M12%200c-6.626%200-12%205.373-12%2012%200%205.302%203.438%209.8%208.207%2011.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729%201.205.084%201.839%201.237%201.839%201.237%201.07%201.834%202.807%201.304%203.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931%200-1.311.469-2.381%201.236-3.221-.124-.303-.535-1.524.117-3.176%200%200%201.008-.322%203.301%201.23.957-.266%201.983-.399%203.003-.404%201.02.005%202.047.138%203.006.404%202.291-1.552%203.297-1.23%203.297-1.23.653%201.653.242%202.874.118%203.176.77.84%201.235%201.911%201.235%203.221%200%204.609-2.807%205.624-5.479%205.921.43.372.823%201.102.823%202.222v3.293c0%20.319.192.694.801.576%204.765-1.589%208.199-6.086%208.199-11.386%200-6.627-5.373-12-12-12z'/%3E%3C/svg%3E" 
@@ -37,7 +110,7 @@ export default function StartPage() {
         </div>
         <div className={styles.projects}>
           <div className={styles.sectionHeadline}>Aktuelle Projekte</div>
-          <div className={styles.projectList}>
+          <div className={styles.projectList} ref={projectsRef}>
             <ProjectCard 
               title="DemoShop"
               description="Ein Demo-Shop."
